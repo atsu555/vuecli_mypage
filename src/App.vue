7a5d57c20f2933817538f8cfc15a1fcf7d8ec06f
kikkawa <kikkawa@gmail.com>
2021-01-27 03:12:42 +0900

    vuehp

--- src/App.vue
+++ src/App.vue
@@ -23,8 +23,4 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@200;300;400;500;600;700;900&display=swap');
 @import "./css/style.css";
-body{
-  background-color: black;
-  color: #fff;
-}
 </style>
